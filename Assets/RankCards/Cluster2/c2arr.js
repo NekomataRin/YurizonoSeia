7130e68913053a47b91f38db15e346417e7e45bf
NekomataRin <takinaka2905@gmail.com>
2025-04-12 09:47:59 +0700

    update-c1arr.js

--- Assets/RankCards/Cluster2/c2arr.js
+++ Assets/RankCards/Cluster2/c2arr.js
@@ -20,7 +20,7 @@ const Cluster2_Assets = [
     [
         31,
         'castorice',
-        '.Assets/RankCards/Cluster2/Image/RankCard_31.png',
+        './Assets/RankCards/Cluster2/Image/RankCard_31.png',
         '#b84dff',
         'https://cdn.discordapp.com/emojis/1360440195966570701.png?size=512&quality=lossless',
         '<:CastoriceSip:1360440195966570701>',
