fb3a73c1fe1baab6f9098ec65eea324112ae3875
NekomataRin <takinaka2905@gmail.com>
2025-04-27 01:30:21 +0700

    20250427-0125-minor-debug

--- Commands/Fun/omikuji.js
+++ Commands/Fun/omikuji.js
@@ -34,7 +34,7 @@ module.exports = {
         }
         const Daily_CD = DailyCD()
 
-        const channelid = '1356147272030879885' //Debug: Only Remove When Testing //'1358995524551839805'
+        const channelid = '1358995524551839805' //'1356147272030879885' //Debug: Only Remove When Testing 
         if (interaction.channel.id !== channelid) {
             const ErrEmbed = new EmbedBuilder()
                 .setColor('Red')
